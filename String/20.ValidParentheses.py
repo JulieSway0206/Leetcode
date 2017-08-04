a95219d4eb1f1057f5354b2f0aff41f72504ec13
JulieSway0206 <cui.jie0206@gmail.com>
2017-08-04 11:47:40 -0400

    stack

---
+++ String/20.ValidParentheses.py
@@ -0,0 +1,5 @@
+"""
+Given a string containing just the characters '(',')','{','}','[' and ']', determine
+if the input string is valid. The brackets must close in correct order,"({})" and "()[]"
+are all valid, but "[{}" is not.
+"""
